12eba4fe8a7cbc5afdc0cd77badf5926f5e6ae3a
shahriaridris7 <shahriaridris007@gmail.com>
2026-09-13 00:59:40 +0600

    nothing

--- src/component/Technology.tsx
+++ src/component/Technology.tsx
@@ -1,17 +1,24 @@
 import React, { use } from 'react';
 import type { technologyType } from '../type/technologyType';
 import All from './All';
+import Selected from './Selected';
 
 
 interface technologiesProps{
    technologiesPromise: Promise<technologyType[]>
 }
+
+
 const Technology = ({technologiesPromise}:technologiesProps) => {
      console.log(technologiesPromise);
      const technology=use(technologiesPromise);
     return (
-        <div>
-            <All technology={technology}></All>
+        <div className='grid grid-cols-4 container mx-auto'>
+            <div className='col-span-3'>
+                 <All  technology={technology}></All>
+            </div>
+            <div className='col-span-1'><Selected/></div>
+            
         </div>
     );
 };
